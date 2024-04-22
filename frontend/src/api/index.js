347fa11e166c5fdcfbb3d24e8eba7915824cbf01
The-TanQ <tisetsoramolobeng@gmail.com>
2024-04-22 21:52:12 +0200

    Change api endpoint

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,1 +1,2 @@
-export const API_ENDPOINT = "http://localhost:3000";
+export const API_ENDPOINT = "https://restful-restaurants-backend.onrender.com";
+//export const API_ENDPOINT = "http://localhost:3000";
